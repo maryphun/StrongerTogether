ffd5c2dcd5dd4eb87d5df990ce8967b5edf00640
Mary <phunmung@gmail.com>
2021-02-20 21:38:06 +0900

    some script so you can finally beat the game

--- Assets/Scripts/FMODAudioManager.cs
+++ Assets/Scripts/FMODAudioManager.cs
@@ -42,6 +42,12 @@ class FMODAudioManager : MonoBehaviour
         return scoreParam;
     }
 
+    public float ChangeScore(float num, float maxValueLimitation)
+    {
+        scoreParam = Mathf.Max(Mathf.Min(scoreParam + num, maxValueLimitation), 0f);
+        return scoreParam;
+    }
+
     public float SetScore(float num)
     {
         scoreParam = Mathf.Max(Mathf.Min(num, 1f), 0f);
